5086c393ee7156830f97174222482574ac5c62c1
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2022-03-10 19:05:42 -0600

    first commit

---
+++ src/main/java/mx/fmre/afiliacionesapi/AfiliacionesapiApplication.java
@@ -0,0 +1,13 @@
+package mx.fmre.afiliacionesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfiliacionesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfiliacionesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/fmre/afiliacionesapi/AfiliacionesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.fmre.afiliacionesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfiliacionesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
