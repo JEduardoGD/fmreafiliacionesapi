2cf3b1c48ab0b7db81418b7cb888c22ef333fdac
Eduardo G <eduardo_gd@hotmail.com>
2022-03-12 07:24:41 -0600

    fix controller bis

--- src/main/java/mx/fmre/afiliacionesapi/controller/AsocEstatalController.java
+++ src/main/java/mx/fmre/afiliacionesapi/controller/AsocEstatalController.java
@@ -3,17 +3,21 @@ package mx.fmre.afiliacionesapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import mx.fmre.afiliacionesapi.entity.AsocEstatal;
 import mx.fmre.afiliacionesapi.service.AsocEstatalService;
 
-@Controller
+@RestController
+@RequestMapping("/asocestatal")
 public class AsocEstatalController {
 
 	@Autowired
 	AsocEstatalService asocEstatalService;
 
+	@GetMapping
 	public List<AsocEstatal> findAll() {
 		return asocEstatalService.findAll();
 	}
